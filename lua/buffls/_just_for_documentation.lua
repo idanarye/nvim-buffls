c3a9a9447ea02b77b33587c1e99e60af7d26a8bd
Idan Arye <IdanArye@gmail.com>
2022-12-21 18:10:56 +0200

    Document the usage

--- lua/buffls/_just_for_documentation.lua
+++ lua/buffls/_just_for_documentation.lua
@@ -2,12 +2,28 @@
 ---@brief [[
 ---Register the BuffLS source in null-ls:
 --->
----    require('null-ls').setup {
+---    require'null-ls'.setup {
 ---        sources = {
----            require('buffls'),
+---            require'buffls',
 ---        };
 ---    }
 ---<
+---@brief ]]
+
+---@mod buffls-basic-usage BuffLS basic usage
+---@brief [[
+---1. Create a buffer and set it to the appropriate langauge
+---2. Create a BuffLS for that buffer: >
+---       local bufnr = vim.api.nvim_get_current_buf()
+---       local ls = require'buffls/TsLs':for_buffer(bufnr)
+---<   Choose the appropriate BuffLS class:
+---   - *BufflsTsLs* for using manually written TreeSitter queries. (like in
+---     the example)
+---   - *BufflsForBash* - a subclass of *BufflsTsLs* with helpers that work
+---     with Bash flags.
+---   - Custom and/or 3rd party subclasses?
+---3. Use `ls` to configure the BuffLS behavior for that buffer (see the
+---   documentation of `ls`'s class)
 ---@brief ]]
 
 local M = {}
