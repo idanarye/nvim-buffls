3aa2c7ec797e2c05356bdcc8bc1f747b2787bccb
Idan Arye <IdanArye@gmail.com>
2022-12-25 15:56:35 +0200

    Document `BufflsTsQueryHandlerContext`

--- lua/buffls/TsQueryHandlerContext.lua
+++ lua/buffls/TsQueryHandlerContext.lua
@@ -2,18 +2,24 @@
 
 local util = require'buffls.util'
 
+---The context passed to |BufflsTsLs| query-based generators.
+---
+---Generators registered in |BufflsTsLs:add_ts_generator| receive a context
+---object of this type, which they can use to access data from the query's
+---match.
 ---@class BufflsTsQueryHandlerContext
----@field params table
----@field tstree userdata
----@field nodes {[string]: userdata}
----@field metadata table
+---@field params table the null-ls parameters object
+---@field tstree userdata the entire TS tree of the buffer
+---@field metadata table the TS query's metadata
+---@field nodes {[string]: userdata} the nodes matched by the TS query
 local BufflsTsQueryHandlerContext = {}
 
 ---@private
 BufflsTsQueryHandlerContext.__index = BufflsTsQueryHandlerContext
 
+---Resolve a TS node to the text it represents.
 ---@param node userdata|string The TreeSitter node to resolve, or the name of that match in self.nodes
----@return string?
+---@return string
 function BufflsTsQueryHandlerContext:text(node)
     if type(node) == 'string' then
         node = self.nodes[node]
@@ -26,6 +32,9 @@ local function node_range(node)
     return sr + 1, sc + 1, er + 1, ec + 1
 end
 
+---Check if a the cursor if within a node.
+---
+---For LSP requests that send a range, this checks if the node overlaps with the range.
 ---@param node userdata|string The TreeSitter node to resolve, or the name of that match in self.nodes
 ---@return boolean
 function BufflsTsQueryHandlerContext:is_node_in_range(node)
@@ -49,6 +58,9 @@ function BufflsTsQueryHandlerContext:is_node_in_range(node)
     return true
 end
 
+---Check if a the cursor if after a node.
+---@param node userdata|string The TreeSitter node to resolve, or the name of that match in self.nodes
+---@return boolean
 function BufflsTsQueryHandlerContext:is_after_node(node)
     if type(node) == 'string' then
         node = self.nodes[node]
